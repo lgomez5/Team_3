ceb2b2b6e9ddbefcd3070e3e0af9a62a2c1ba291
lgomez5 <39140489+lgomez5@users.noreply.github.com>
2019-11-13 15:45:15 -0500

    Add files via upload

---
+++ Team3/Team3/Views/StudentHome.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Team3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StudentHome : ContentPage
+    {
+        string status;
+        public StudentHome()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+
+        }
+        public async void LogoutBtnClick(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new LoginPage());
+        }
+        public async void PendingAssignment_BtnClick(object sender, EventArgs e)
+        {
+            status = "pending";
+            await Navigation.PushModalAsync(new AssignmentList(status));
+        }
+        public async void CompletedAssignment_BtnClick(object sender, EventArgs e)
+        {
+            status = "completed";
+            await Navigation.PushModalAsync(new AssignmentList(status));
+        }
+        public async void CourseInfo_BtnClick(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "Please add link to Course Information page", "OK");
+        }
+    }
+}
